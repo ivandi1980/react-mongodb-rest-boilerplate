080da6d8043412876bd6b7260266cc32a02d8225
jefelewis <jeffylew77@gmail.com>
2018-03-15 16:09:45 -0700

    Router skeleton

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-// Requirements: Express
+// Imports: Express
 import express from 'express';
 import router from './router.js';
 
 // Initialize Express App
 const app = express();
 
-// Requirements: Middleware
+// Imports: Middleware
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
@@ -17,7 +17,7 @@ import bodyParser from 'body-parser';
 
 
 // Use: Router
-
+app.use('./router.js');
 
 // Define: Port
 const port = 3000;
---
+++ server/router.js
@@ -0,0 +1,9 @@
+// Requirements: Express Router
+const router = require('express').Router();
+
+
+// Imports: Database Controller
+import sqlController from './controllers/sqlcontroller.js';
+import mongoController from './controllers/mongocontroller.js';
+
+// Imports: API Controller
