21c3c7075f916fa6efb8a9ab49de79ba6f157868
jefelewis <jeffylew77@gmail.com>
2018-03-15 17:30:33 -0700

    SQL controller skeleton complete

---
+++ server/controllers/sqlController.js
@@ -0,0 +1,13 @@
+// Imports: Mongoose
+import sequelize from 'sequelize';
+
+// Imports: Database
+import sqlDatabase from '../database/sql';
+
+
+
+
+/***************************** SQL Query Functions *****************************/
+
+
+/***************************** SQL Query Functions *****************************/
