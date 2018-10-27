351932401fba1417c31cae489b02070ddffdfe42
Jeff Lewis <jeffylew77@gmail.com>
2018-10-26 17:44:14 -0700

    [Dev] Updating to ES6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,18 @@
-// Imports: React
+// Imports: Dependencies
 import React, { Component } from 'react';
 
-// Imports: Apollo
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
 // Imports: Components
-
-
-// Setup: Apollo
-const CLIENT = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-});
-
 
 // React Application
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={CLIENT}>
-        <div id="main">
+      <div id="main">
 
-        </div>
-      </ApolloProvider>
+      </div>
     );
   }
 }
-
 
 // Exports
 export default App;
